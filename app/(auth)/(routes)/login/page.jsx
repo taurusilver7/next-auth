a4b902a23b6f656dc9e3f41b4abd570bfca13ac8
Devi prasad <prasad20697@gmail.com>
2023-12-27 12:38:13 +0530

    login page

--- app/(auth)/(routes)/login/page.jsx
+++ app/(auth)/(routes)/login/page.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+	return (
+		<div>
+			<div className="min-h-screen w-full flex items-center justify-center grade">
+				<div className="w-1/3 mx-auto p-12 relative rounded-md justify-center items-center box">
+					<h2 className="text-2xl text-center leading-3 mb-10 text-gray-100">
+						Login
+					</h2>
+					<div className="mb-3">
+						<Label htmlFor="email">Email</Label>
+						<Input
+							type="email"
+							name="email"
+							placeholder="Enter your email address"
+						/>
+					</div>
+					<div className="mb-3">
+						<Label htmlFor="password">Password</Label>
+						<Input
+							type="password"
+							name="password"
+							placeholder="Enter your password."
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default LoginPage
+export default LoginPage;
