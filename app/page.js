11b41db87accc3e5895219199385e8a63c8c051c
Devi prasad <prasad20697@gmail.com>
2023-12-27 12:37:57 +0530

    marketing page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
+import Contact from "@/components/contact";
+import Feature from "@/components/feature";
+import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
-  return (
-    <main className="">
-   
-    Full Custom Authentication
-
-    </main>
-  )
+	return (
+		<div className="min-h-screen flex flex-col">
+			<Navbar />
+			<main className="mx-auto max-w-7xl">
+				<Hero />
+				<Feature />
+				<Contact />
+			</main>
+			<Footer />
+		</div>
+	);
 }
