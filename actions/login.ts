8b2ad2745c80b0e3ec62148804b2d5e545641a5c
Devi prasad <prasad20697@gmail.com>
2024-01-01 22:28:43 +0530

    server side validation

--- actions/login.ts
+++ actions/login.ts
@@ -1,5 +1,14 @@
 "use server";
 
-export const login = (values:any) => {
-   console.log(values);
-}
+import { LoginSchema } from "@/schemas";
+import * as z from "zod";
+
+export const login = (values: z.infer<typeof LoginSchema>) => {
+	const validatedFields = LoginSchema.safeParse(values);
+
+	if (!validatedFields.success) {
+		return { error: "Invalid Credentials!!" };
+	}
+
+	return { success: "Email sent!" };
+};
