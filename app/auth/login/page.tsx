58ac5041e4188448b46ae806e918e20eff4e0a3a
Devi prasad <prasad20697@gmail.com>
2024-01-02 17:00:50 +0530

    auth pages

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,20 +1,12 @@
+import ExitButton from "@/components/auth/exit-button";
 import { LoginForm } from "@/components/auth/login-form";
-import { Button } from "@/components/ui/button";
-import { FaArrowLeft } from "react-icons/fa";
-import Link from "next/link";
 
 const Login = () => {
 	return (
 		<div className="relative">
 			<LoginForm />
 
-			<div className="absolute top-1 left-0">
-				<Button size="sm" variant="ghost">
-					<Link href={"/"}>
-						<FaArrowLeft className="h-4 w-4 text-gray-500" />
-					</Link>
-				</Button>
-			</div>
+			<ExitButton />
 		</div>
 	);
 };
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,20 +1,12 @@
 import { RegisterForm } from "@/components/auth/register-form";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { FaArrowLeft } from "react-icons/fa";
+import ExitButton from "@/components/auth/exit-button";
 
 const Register = () => {
 	return (
 		<div className="relative">
 			<RegisterForm />
 
-			<div className="absolute top-1 left-0">
-				<Button size="sm" variant="ghost">
-					<Link href={"/"}>
-						<FaArrowLeft className="h-4 w-4 text-gray-500" />
-					</Link>
-				</Button>
-			</div>
+			<ExitButton />
 		</div>
 	);
 };
