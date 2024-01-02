81c8fe3c1c0208a07270133fc1087a551a37df53
Devi prasad <prasad20697@gmail.com>
2024-01-02 13:10:17 +0530

    title added to header

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -8,13 +8,14 @@ const font = Poppins({
 
 interface HeaderProps {
 	label: string;
+	title: string;
 }
 
-export const Header = ({ label }: HeaderProps) => {
+export const Header = ({ title, label }: HeaderProps) => {
 	return (
 		<div className="w-full flex flex-col gap-y-4 items-center justify-center">
 			<h1 className={cn("text-3xl font-semibold", font.className)}>
-				🔏 Login
+				🔏 {title}
 			</h1>
 			<p className="text-muted-foreground text-sm">{label}</p>
 		</div>
