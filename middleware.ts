496b608822bc740f48a56fe1f8d1347cf8126b56
Devi prasad <prasad20697@gmail.com>
2024-01-04 07:12:56 +0530

    middleware invoked

--- middleware.ts
+++ middleware.ts
@@ -14,10 +14,23 @@ export default auth((req) => {
 	const isLoggedIn = !!req.auth;
 
 	const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+	const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+	const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-	console.log("ROUTE:", req.nextUrl.pathname);
+	if (isApiAuthRoute) {
+		return null;
+	}
+	if (isAuthRoute) {
+		if (isLoggedIn) {
+			return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+		}
+		return null;
+	}
 
-	console.log("IS LOGGED IN:", isLoggedIn);
+	if (!isLoggedIn && !isPublicRoute) {
+		return Response.redirect(new URL("/auth/login", nextUrl));
+	}
+	return null;
 });
 
 // Optionally, don't invoke Middleware on some paths
