e20c4fbce7a17bbd933129ff00284cb638db879c
Devi prasad <prasad20697@gmail.com>
2024-01-04 07:13:58 +0530

    sample settings

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,10 +1,26 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 const Settings = async () => {
 	const session = await auth();
 	return (
-		<div className="flex h-full justify-center items-center text-5xl">
-			Settings {JSON.stringify(session)}
+		<div className="flex flex-col h-full gap-4 justify-center items-center text-2xl">
+			<div>user credentials: {JSON.stringify(session?.user?.name)}</div>
+			<div className="block">
+				email: {JSON.stringify(session?.user?.email)}
+			</div>
+
+			<form
+				action={async () => {
+					"use server";
+
+					await signOut();
+				}}
+			>
+				<Button type="submit" variant="ghost" size="lg">
+					Sign Out
+				</Button>
+			</form>
 		</div>
 	);
 };
