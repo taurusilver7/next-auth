36e557c02d24ce37f5e07de181af4001cfde9f65
Devi prasad <prasad20697@gmail.com>
2024-01-04 07:12:10 +0530

    auth middleware

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,34 @@
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 
+import { LoginSchema } from "./schemas";
 import type { NextAuthConfig } from "next-auth";
+import { getUserByEmail } from "@/util/user";
+import bcrypt from "bcryptjs";
 
 export default {
-	providers: [GitHub, Google],
+	providers: [
+		Credentials({
+			async authorize(credentials) {
+				const validatedFields = LoginSchema.safeParse(credentials);
+
+				if (validatedFields.success) {
+					const { email, password } = validatedFields.data;
+
+					const user = await getUserByEmail(email);
+
+					if (!user || !user.password) return null;
+
+					const passwordMatch = await bcrypt.compare(
+						password,
+						user.password
+					);
+
+					if (passwordMatch) return user;
+				}
+				return null;
+			},
+		}),
+	],
 } satisfies NextAuthConfig;
