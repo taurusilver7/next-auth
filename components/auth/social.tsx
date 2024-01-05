0c3d6bbca50478840ea9e066b1ecc1cdc86a6419
Devi prasad <prasad20697@gmail.com>
2024-01-05 16:54:31 +0530

    social auth event functions

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -4,15 +4,23 @@ import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 const Social = () => {
+
+	const onClick = (provider: "google" | "github") => {
+		signIn(provider, {
+			callbackUrl: DEFAULT_LOGIN_REDIRECT
+		})
+	} 
 	return (
 		<div className="flex items-center w-full gap-x-2">
 			<Button
 				size="lg"
 				className="w-full bg-slate-100"
 				variant="outline"
-				onClick={() => {}}
+				onClick={() => onClick("google")}
 			>
 				<FcGoogle className="w-5 h-5" />
 			</Button>
@@ -20,7 +28,7 @@ const Social = () => {
 				size="lg"
 				className="w-full bg-slate-100"
 				variant="outline"
-				onClick={() => {}}
+				onClick={() => onClick("github")}
 			>
 				<FaGithub className="w-5 h-5" />
 			</Button>
