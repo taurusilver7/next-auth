d26d765a913a409bd1f37102e5829929e4bdb84c
Devi prasad <prasad20697@gmail.com>
2024-01-06 23:09:07 +0530

    error card component

---
+++ components/auth/error-card.tsx
@@ -0,0 +1,20 @@
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+import { Header } from "./header";
+import { BackButton } from "./back-button";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+
+export const ErrorCard = () => {
+	return (
+		<Card className="w-96 shadow-md">
+			<CardHeader>
+				<Header label="Oops! Something went wrong!" title="" icon={false} />
+			</CardHeader>
+      <div className="w-full flex justify-center items-center">
+         <ExclamationTriangleIcon className='text-destructive' />
+      </div>
+			<CardFooter>
+				<BackButton label="Back to login" href="/auth/login" />
+			</CardFooter>
+		</Card>
+	);
+};
