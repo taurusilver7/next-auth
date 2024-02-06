85af25092d779b5ed7b5f57ca41283c16b65cdee
Devi prasad <prasad20697@gmail.com>
2024-02-06 17:00:45 +0530

    misc

--- auth.ts
+++ auth.ts
@@ -34,19 +34,11 @@ export const {
 			// Prevent sign-in without email verification
 			if (!existingUser?.emailVerified) return false;
 
-			// ToDO : Add 2FA check.
-
-			return true;
-
-			// if (!existingUser || !existingUser.emailVerified) {
-			// 	return false;
-			// }
+			// TODO : Add 2FA check.
 
 			return true;
 		},
 		async session({ token, session }) {
-			// console.log({ sessionToken: token, session });
-
 			if (session.user && token.sub) {
 				session.user.id = token.sub;
 			}
@@ -57,7 +49,6 @@ export const {
 			return session;
 		},
 		async jwt({ token }) {
-			// console.log({ token });
 			if (!token.sub) return token;
 
 			const existingUser = await getUserById(token.sub);
