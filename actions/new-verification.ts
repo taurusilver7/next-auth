f6295ea9363009178b342f384a3d50c3d4374dc9
Devi prasad <prasad20697@gmail.com>
2024-02-08 00:19:30 +0530

    new verification token expiry check

---
+++ actions/new-verification.ts
@@ -0,0 +1,6 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/util/user";
+import { getVerificationTokenByToken } from "@/util/verification-token";
+getVerificationTokenByToken;
