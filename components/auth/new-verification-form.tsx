32dee038377243008aa5b1f3bf1389425b281bc8
Devi prasad <prasad20697@gmail.com>
2024-02-08 00:19:39 +0530

    verification form

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,17 +1,32 @@
 "use client";
 
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { BeatLoader, ClimbingBoxLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
 
 const NewVerificationForm = () => {
+	const searchParams = useSearchParams();
+
+	const token = searchParams.get("token");
+	const onSubmit = useCallback(() => {
+		// Check if the token has expired
+
+	}, [token]);
+
+	useEffect(() => {
+		onSubmit();
+	}, [onSubmit]);
 	return (
 		<CardWrapper
 			headerTitle="Verification"
-			headerLabel="Confirming your verification"
+			headerLabel="Verify it's you"
 			backButtonHref="/auth/login"
 			backButtonLabel="Back to login"
 		>
-			<div className="flex items-center w-full justify-center"></div>
+			<div className="flex items-center w-full justify-center">
+				<BeatLoader />
+			</div>
 		</CardWrapper>
 	);
 };
