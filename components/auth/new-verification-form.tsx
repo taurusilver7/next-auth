6530c00880dbe922ae0bd7ad9f72cf3c35be6f6c
Devi prasad <prasad20697@gmail.com>
2024-02-13 07:23:58 +0530

    expiry check added to verification form

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,17 +1,32 @@
 "use client";
 
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { CardWrapper } from "@/components/auth/card-wrapper";
 import { BeatLoader, ClimbingBoxLoader } from "react-spinners";
 import { useSearchParams } from "next/navigation";
+import { newVerification } from "@/actions/new-verification";
 
 const NewVerificationForm = () => {
 	const searchParams = useSearchParams();
+	const [error, setError] = useState<string | undefined>();
+	const [success, setSuccess] = useState<string | undefined>();
 
 	const token = searchParams.get("token");
 	const onSubmit = useCallback(() => {
-		// Check if the token has expired
+		if (!token) {
+			setError("Missing Token");
+			return;
+		}
 
+		// Check if the token has expired
+		newVerification(token)
+			.then((data) => {
+				setSuccess(data.success);
+				setError(data.error);
+			})
+			.catch(() => {
+				setError("Something went wrong!");
+			});
 	}, [token]);
 
 	useEffect(() => {
